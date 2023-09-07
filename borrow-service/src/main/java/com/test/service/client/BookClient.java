8e41e1cc797022fc1d92c3e1293c8a9cd32b94a0
Roway12 <luowei98cn@163.com>
2023-09-08 00:00:13 +0800

    add Eureka register center and loadbalance

---
+++ borrow-service/src/main/java/com/test/service/client/BookClient.java
@@ -0,0 +1,13 @@
+package com.test.service.client;
+
+import com.test.entity.Book;
+import com.test.entity.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("bookservice")   //声明为bookservice服务的HTTP请求客户端
+public interface BookClient {
+    @RequestMapping("/book/{bid}")
+    Book findBookById(@PathVariable("bid") int bid);
+}
